f9b33bedad8708c9faf24eae1f2c39ee2d27a042
thelscon <thelscon@gmail.com>
2021-07-01 15:38:36 +0300

    update 7

--- script.js
+++ script.js
@@ -16,16 +16,7 @@ buttonStart.onclick = function () {
     
 }
 
-const evenNumber = function ( value ) {
-
-    if( value % 2 === 0 ) {
-        return `Введённое число ${value} - чётное число.` ;
-    }
-    else {
-        return `Введённое число ${value} - нечётное число.` ;
-    }
-    
-}
+const evenNumber = ( value ) => ( value % 2 === 0 ) ? `Введённое число ${value} - чётное число.` : `Введённое число ${value} - нечётное число.` ;
 
 const simpleNumber = function ( value ) {
     
